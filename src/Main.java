c059af80f0025bed9e8693dab5216af51832da49
Anqi Tu <anqitu@outlook.com>
2017-12-14 20:04:14 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, ClassNotFoundException, SQLException {
+        OrderManagement order = new OrderManagement();
+        order.sendMarketOrder("AAPL", "BUY");
+
+    }
+}
